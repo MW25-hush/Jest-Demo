6465a785fe4eb6bf6354243a7d2d9dac77fa9e39
M@W <mehdicr7wahid@gmail.com>
2024-05-17 21:46:54 +0430

    worked with animations in framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,23 @@
 "use client";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { exit } from "process";
 
+const variant = {
+  animate: {
+    x: [0, 100, 0],
+    y: [0, 100, 0],
+  },
+};
 export default function Home() {
-  const [visible, setVisible] = useState(false);
-
   return (
-    <div>
-      <motion.h1
-        className="bg-cyan-500 py-2 px-4 text-[2rem]"
-        animate={{ fontSize: 10, color: "#ff2994", x: 100, y: 100 }}
-      >
-        Home
-      </motion.h1>
-
-      <section>
-        <p>Text String Test</p>
-      </section>
-
-      <motion.div>
-        <button onClick={() => setVisible(true)}>Show Heading</button>
-        {visible && <h1>This is a visible heading</h1>}
-      </motion.div>
+    <div className="flex justify-center items-center h-screen">
+      <motion.div
+        variants={variant}
+        initial="initialState"
+        animate="animate"
+        className="h-20 w-20 bg-red-400"
+      ></motion.div>
     </div>
   );
 }
